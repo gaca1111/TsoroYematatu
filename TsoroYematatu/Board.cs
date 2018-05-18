bb5348f13e9e5df6c78cde7c4c1d7fe83be3b865
gaca1111 <gacqq@wp.pl>
2018-05-18 23:00:16 +0200

    Szkielet struktur

---
+++ TsoroYematatu/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsoroYematatu {
+
+    public enum Pawn { Black, White, Empty };
+
+    public class Line {
+
+        private int base_line_weight;
+
+        private Pawn[] line_state;
+    }
+
+
+    public class Board {
+
+        private int[,] board_field_weight = new int[3, 3] {{1,1,1},{1,1,1},{1,1,1}};
+
+
+        private Pawn[,] board_state = new Pawn[3, 3];
+
+    }
+}
---
+++ TsoroYematatu/Line_Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsoroYematatu {
+    class Line_Base {
+
+        Line[] line_base_array;
+    }
+}
