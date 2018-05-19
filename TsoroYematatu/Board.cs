993d6475539c25683e04cecbb8242f9ced419f67
gaca1111 <gacqq@wp.pl>
2018-05-19 15:39:44 +0200

    Baza linii gotowa

--- TsoroYematatu/Board.cs
+++ TsoroYematatu/Board.cs
@@ -10,9 +10,33 @@ namespace TsoroYematatu {
 
     public class Line {
 
+        private static int line_state_size = 3;
+
         private int base_line_weight;
 
         private Pawn[] line_state;
+
+        public Line(Pawn first_pawn, Pawn second_pawn, Pawn third_pawn) {
+
+            line_state = new Pawn[line_state_size];
+
+            line_state[0] = first_pawn;
+            line_state[1] = second_pawn;
+            line_state[2] = third_pawn;
+        }
+
+        public int Base_line_weight
+        {
+            get
+            {
+                return base_line_weight;
+            }
+
+            set
+            {
+                base_line_weight = value;
+            }
+        }
     }
 
 
--- TsoroYematatu/Line_Base.cs
+++ TsoroYematatu/Line_Base.cs
@@ -5,8 +5,73 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TsoroYematatu {
-    class Line_Base {
+    public class Line_Base {
+
+        private static int line_base_array_size = 27;
 
         Line[] line_base_array;
+
+        public void Setup_Line_Base() {
+
+            line_base_array = new Line[line_base_array_size];
+
+            // >3 takie same<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.Empty, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.White, Pawn.White, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.Black, Pawn.Black, 1);
+
+            // >1 czarny 2 puste<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.Empty, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.Black, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.Empty, Pawn.Black, 1);
+
+            // >1 bialy 2 puste<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.Empty, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.White, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.Empty, Pawn.White, 1);
+
+            // >2 czarne 1 pusty<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.Black, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.Black, Pawn.Black, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.Empty, Pawn.Black, 1);
+
+            // >2 biale 1 pusty<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.White, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.White, Pawn.White, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.Empty, Pawn.White, 1);
+
+            // >czarny bialy pusty<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.White, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.Black, Pawn.Empty, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.Black, Pawn.White, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Empty, Pawn.White, Pawn.Black, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.Empty, Pawn.White, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.Empty, Pawn.Black, 1);
+
+            // >2 czarne 1 bialy<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.Black, Pawn.White, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.Black, Pawn.Black, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.White, Pawn.Black, 1);
+
+            // >2 biale 1 czarny<
+
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.White, Pawn.Black, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.Black, Pawn.White, Pawn.White, 1);
+            Add_To_Line_Base_Array(line_base_array.Length, Pawn.White, Pawn.Black, Pawn.White, 1);
+        }
+
+        private void Add_To_Line_Base_Array(int position, Pawn first_pawn, Pawn second_pawn, Pawn third_pawn, int base_weight) {
+
+            line_base_array[position] = new Line(first_pawn, second_pawn, third_pawn);
+            line_base_array[position].Base_line_weight = base_weight;
+        }
     }
 }
+   
